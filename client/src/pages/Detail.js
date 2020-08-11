86876ab750ea8a349c5fcbdce39920731082799d
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-10 21:04:46 -0500

    create layout for shopping cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -8,6 +8,8 @@ import spinner from '../assets/spinner.gif'
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
 
+import Cart from '../components/Cart';
+
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
@@ -64,6 +66,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
