6158d68d8261109f3256a8e6f1629061e45f35ca
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-10 22:16:59 -0500

    add items to cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,7 +6,12 @@ import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 
 import Cart from '../components/Cart';
 
@@ -19,6 +24,13 @@ function Detail() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   const { products } = state;
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
 
   useEffect(() => {
     if (products.length) {
@@ -49,7 +61,7 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
+            <button onClick={addToCart}>
               Add to Cart
             </button>
             <button>
