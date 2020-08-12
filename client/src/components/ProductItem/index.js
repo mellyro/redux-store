2983706744a05246a2090729d8a2c0ffb3269191
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-08-12 09:26:07 -0500

    fix productitem bug

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,31 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 function ProductItem(item) {
-  const { cart } = state;
+  const {
+    image,
+    name,
+    _id,
+    price,
+    quantity
+  } = item;
 
-  const addToCart = () => {
+  const [state, dispatch] = useStoreContext();
+
+  const {cart} = state;
+
+   const addToCart = ()=> {
     // find the cart item with the matching id
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+  const itemInCart = cart.find((cartItem) => cartItem._id === _id);
 
-    // if there was a match, call UPDATE with a new purchase quantity
-    if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
-    } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
-      });
-    }
-  };
+  //if match call update
+   if(itemInCart) {
+     dispatch({
+       type: UPDATE_CART_QUANTITY,
+       _id: _id,
+       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+     });
+   }else{
+    dispatch({
+      type: ADD_TO_CART,
+      product: {...item, purchaseQuantity: 1}
+    });
+   }
+   };
 
   return (
     <div className="card px-1 py-1">
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -8,6 +8,8 @@ import spinner from "../../assets/spinner.gif"
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 
+import { idbPromise } from "../../utils/helpers";
+
 function ProductList({ }) {
   const [state, dispatch] = useStoreContext();
 
@@ -16,13 +18,20 @@ function ProductList({ }) {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
+    // if there's data to be stored
     if (data) {
+      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+  
+      // but let's also take each product and save it to IndexedDB using the helper function 
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
