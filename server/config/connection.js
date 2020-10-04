1d6088aeebeb2ee491a39f0021de24c04f30aee0
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-10-04 16:40:32 -0500

    update mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb+srv://missadaska19:merrychristmas@reduxstore.reol2.mongodb.net/reduxstore?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://merrychristmas:merrychristmas@reduxshop.reol2.mongodb.net/reduxshop?retryWrites=true&w=majority'
 
 mongoose.connect(MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
