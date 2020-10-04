6cdb4b0a8af21db21fde9acfd6c17522d6c774ef
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-10-04 17:21:55 -0500

    fix mongodb issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb+srv://merrychristmas:merrychristmas@reduxshop.reol2.mongodb.net/reduxshop?retryWrites=true&w=majority'
+mongodb+srv://merrychristmas:merrychristmas@reduxshop.reol2.mongodb.net/reduxshop?retryWrites=true&w=majority
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
