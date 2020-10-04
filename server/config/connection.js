6120ab6bc69cc33b2d588d089163d332dbc2e80f
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-10-04 17:26:55 -0500

    attempt mongodb fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongodb+srv://merrychristmas:merrychristmas@reduxshop.reol2.mongodb.net/reduxshop?retryWrites=true&w=majority
+// mongodb+srv://merrychristmas:merrychristmas@reduxshop.reol2.mongodb.net/reduxshop?retryWrites=true&w=majority
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
